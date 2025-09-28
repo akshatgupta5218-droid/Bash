3d5aa759d7015f24ce52e253aa2619c6fac43fc0
Akshat Shivam <akshatgupta5218@gmail.com>
2025-09-28 13:10:42 +0530

    Code initialized

---
+++ Basic.sh
@@ -0,0 +1,6 @@
+echo "Hello World"
+# git init
+# git add -A
+# git commit -a -m "Code initialized"
+# git remote add origin https://github.com/akshatgupta5218-droid/Bash.git
+# git push -u origin master
